592784226a7136f265841d26af606770730ed7c3
unknown <fred@.(none)>
2009-08-26 18:30:23 +0100

    Add (untested) windows hook code)

--- hooqInjector/WindowsInjector.cpp
+++ hooqInjector/WindowsInjector.cpp
@@ -2,7 +2,12 @@
 
 #include <QCoreApplication>
 #include <QDebug>
+#include <QFile>
+#include <QFileInfo>
 #include <QProcess>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
 namespace Hooq
 {
@@ -44,14 +49,51 @@ void WindowsInjector::attachToSpawnedProcess()
 
 void WindowsInjector::attach(Q_PID processId)
 {
-	qDebug() << Q_FUNC_INFO << "TODO";
-	Q_UNUSED(processId);
+	wchar_t path[_MAX_PATH];
+	void* remotePathAddress;
+	HMODULE hKernel32 = ::GetModuleHandle(L"Kernel32");
+
+	const int pathLength = libraryPath().toWCharArray(path);
+	path[pathLength] = 0;
+
+	// Grab some memory in the remote process to shove the path to the hooq DLL in
+	remotePathAddress = ::VirtualAllocEx(
+		processId->hProcess,
+		NULL,
+		sizeof(path),
+		MEM_COMMIT,
+		PAGE_READWRITE
+	);
+	// Copy the path to that address
+	::WriteProcessMemory(
+		processId->hProcess,
+		remotePathAddress,
+		static_cast<void*>(path),
+		sizeof(path),
+		NULL
+	);
+
+	// Find out where 'LoadLibrary' lives
+	LPTHREAD_START_ROUTINE loadLibraryAddress = reinterpret_cast<LPTHREAD_START_ROUTINE>(::GetProcAddress(hKernel32, "LoadLibraryW"));
+	Q_ASSERT(loadLibraryAddress);
+
+	// Load the library in the remote process
+
+	::CreateRemoteThread(
+		processId->hProcess,
+		NULL, // default attributes
+		0, // default stack size
+		loadLibraryAddress, // function to call
+		remotePathAddress, // argument (wchar_t* to our library path)
+		0, // no creation flags
+		NULL // no thread identifier
+	);
+
 }
 
 QString WindowsInjector::libraryPath()
 {
-	// XXX FIXME XXX
-	return QCoreApplication::applicationDirPath() + "/../../injectedHooq/debug/injectedHooq.dll";
+	return QFileInfo("injectedHooq.dll").canonicalFilePath();
 }
 
 
