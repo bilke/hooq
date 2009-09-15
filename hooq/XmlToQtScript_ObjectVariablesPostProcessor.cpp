61eaf5978e5be478b49a3b8c77a8b70f14cc2d7d
Fred Emmott <mail@fredemmott.co.uk>
2009-09-15 11:02:39 +0100

    add missing files for ObjectVariablesPostProcessor

---
+++ hooq/XmlToQtScript_ObjectVariablesPostProcessor.cpp
@@ -0,0 +1,85 @@
+/*
+	Hooq: Qt4 UI recording, playback, and testing toolkit.
+	Copyright (C) 2009  Mendeley Limited <copyright@mendeley.com>
+	Copyright (C) 2009  Frederick Emmott <mail@fredemmott.co.uk>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+#include "XmlToQtScript_ObjectVariablesPostProcessor.h"
+
+#include <QSet>
+#include <QStringList>
+
+XmlToQtScript::ObjectVariablesPostProcessor::ObjectVariablesPostProcessor()
+{
+}
+
+void XmlToQtScript::ObjectVariablesPostProcessor::process(Item* iterator, QList<Item>* in, QList<Item>* out)
+{
+	Q_UNUSED(in);
+	if(iterator->target.isNull())
+	{
+		return;
+	}
+	if(iterator->target.canConvert<Variable>())
+	{
+		Q_ASSERT(!iterator->target.canConvert<Variable>());
+		return; // huh, what? How did we get here?
+	}
+	const QString target = iterator->target.toString();
+	if(!m_variables.contains(target))
+	{
+		QStringList parts = target.split("::");
+		QString name = toKeyword(parts.takeLast());
+		const QSet<QString> values = m_variables.values().toSet();
+		if(values.contains(name))
+		{
+			if(!parts.isEmpty())
+			{
+				name.prepend(toKeyword(parts.takeLast()) + "__");
+			}
+			if(values.contains(name))
+			{
+				const QString pattern = name + "_%1";
+				int count = 2;
+				while(values.contains(pattern.arg(count)))
+				{
+					++count;
+				}
+				name = pattern.arg(count);
+			}
+		}
+		m_variables.insert(target, name);
+		out->append(Item(QVariant(), QString("var %1 = objectFromPath").arg(name), target));
+	}
+	iterator->target = QVariant::fromValue(Variable(m_variables.value(target)));
+}
+
+QString XmlToQtScript::ObjectVariablesPostProcessor::toKeyword(const QString& in)
+{
+	QString out;
+	Q_FOREACH(const QChar character, in)
+	{
+		if(character.isLetterOrNumber())
+		{
+			out.append(character);
+		}
+		else
+		{
+			out.append("_");
+		}
+	}
+	return out;
+}
